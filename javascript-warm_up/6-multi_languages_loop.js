0e2009b2ae79c7250282e766da8ec517db0e362b
Taste2 <stawiah17@gmail.com>
2023-09-02 13:04:43 +0000

    initialize variables with let

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,10 +3,10 @@
 Write a script that prints 3 lines: (like 1-multi_languages.js)
 but by using an array of string and a loop
 */
-c_ = 'C is fun';
-python_ = 'Python is cool';
-java_ = 'JavaScript is amazing';
-progLanguages = [c_, python_, java_];
+let c_ = 'C is fun';
+let python_ = 'Python is cool';
+let java_ = 'JavaScript is amazing';
+let progLanguages = [c_, python_, java_];
 
 for (let i = 0; i < 3; i++) {
     console.log(progLanguages[i]);
