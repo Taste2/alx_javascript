00dd82eb0673696dcee167a6f7c5bb9b9a0d4a03
Taste2 <stawiah17@gmail.com>
2023-09-02 13:11:06 +0000

    use const instead of let

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,10 +3,10 @@
 Write a script that prints 3 lines: (like 1-multi_languages.js)
 but by using an array of string and a loop
 */
-let c_ = 'C is fun';
-let python_ = 'Python is cool';
-let java_ = 'JavaScript is amazing';
-let progLanguages = [c_, python_, java_];
+const c_ = 'C is fun';
+const python_ = 'Python is cool';
+const java_ = 'JavaScript is amazing';
+const progLanguages = [c_, python_, java_];
 
 for (let i = 0; i < 3; i++) {
     console.log(progLanguages[i]);
