477707d2fad1945871f94561d70fe658f2d38615
Taste2 <stawiah17@gmail.com>
2023-09-02 12:42:49 +0000

    fixing validation error

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-/*Write a script that prints 3 lines:
+/*
+Write a script that prints 3 lines:
 */
-console.log('C is fun\nPython is cool\nJavaScript is amazing')
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
