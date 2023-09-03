33874a8247b5bbeabcc0fb8e81a689ab2aa48c93
Taste2 <stawiah17@gmail.com>
2023-09-03 00:37:25 +0000

    pass arg into called func

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -4,6 +4,6 @@ A function that increments and calls a function.
 */
 
 exports.addMeMaybe = function (number, theFunction) {
-  const number = number + 1;
-  theFunction();
+  number = number + 1;
+  theFunction(number);
 };
