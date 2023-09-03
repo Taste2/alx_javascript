cd90b377c7581db2f90e686b6e2d379d9c79f24a
Taste2 <stawiah17@gmail.com>
2023-09-03 00:49:54 +0000

    fixed indentation

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -4,18 +4,18 @@ Update this script by adding a new
 function incr that increments the integer value.
 */
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
   
-  function incr() {
-    myObject.value += 1;
-  }
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  
+function incr() {
+  myObject.value += 1;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
