ee7c19bf16ecdd00b9701726342858cdd6d2cea5
Taste2 <stawiah17@gmail.com>
2023-09-05 09:45:47 +0000

    task1 first attempt

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+    // class attributes
+    w;
+    h;
+
+    // initialize an instance with these values
+    constructor (w, h) {
+        this.width = w;
+        this.height = h;
+    }
+}
