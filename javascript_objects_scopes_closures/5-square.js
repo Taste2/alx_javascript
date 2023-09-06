b5881ad50bbb9b8f3a23f9aabd21071f579177a1
Taste2 <stawiah17@gmail.com>
2023-09-06 00:45:36 +0000

    task 5 added

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+class Rectangle {
+
+    // initialize an instance with these values
+    constructor (w, h) {
+        if (w && h > 0) {
+            this.width = w;
+            this.height = h;
+        }else {
+            return 'Rectangle {}';
+        }
+    }
+
+    print () {
+        for (let i = 1; i <= this.height; i++) {
+            const rect_X = 'X'.repeat(this.width);
+            console.log(rect_X);
+        }
+    }
+
+    rotate () {
+        const ht = this.height;
+        this.height = this.width;
+        this.width = ht;
+    }
+
+    double () {
+        this.width *= 2;
+        this.height *= 2;
+    }
+}
+
+class Square extends Rectangle {
+
+    constructor (width, height, size) {
+        super(width, height);
+        this.size = size;
+    }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
