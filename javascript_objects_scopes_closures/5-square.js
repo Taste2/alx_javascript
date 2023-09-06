fea180958e897a43a9f451b03d3f732cc4b2bd76
Taste2 <stawiah17@gmail.com>
2023-09-06 09:20:42 +0000

    imports class

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,34 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-
-    // initialize an instance with these values
-    constructor (w, h) {
-        if (w && h > 0) {
-            this.width = w;
-            this.height = h;
-        }else {
-            return 'Rectangle {}';
-        }
-    }
-
-    print () {
-        for (let i = 1; i <= this.height; i++) {
-            const rect_X = 'X'.repeat(this.width);
-            console.log(rect_X);
-        }
-    }
-
-    rotate () {
-        const ht = this.height;
-        this.height = this.width;
-        this.width = ht;
-    }
-
-    double () {
-        this.width *= 2;
-        this.height *= 2;
-    }
-}
+const Rectangle = require ('./4-rectangle');
 
 class Square extends Rectangle {
 
@@ -36,5 +7,9 @@ class Square extends Rectangle {
         super(size, size);
     }
 }
+const s1 = new Square(4);
+s1.print();
+s1.double();
+s1.print();
 
 module.exports = Square;
