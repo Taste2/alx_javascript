ce86e8fb190a3bd0b121d5fa2b1e58800406ce00
Taste2 <stawiah17@gmail.com>
2023-09-10 23:41:58 +0000

    task 0 added

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// import request
+const request = require('request');
+
+// pass cmd line argument
+const url = process.argv[2];
+
+// make a GET request
+request(url, function (response) {
+    console.log('code:', response.satusCode)
+})
