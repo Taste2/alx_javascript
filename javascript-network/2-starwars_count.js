a11c5e530fcfd95baa3479779ee9c2901fe1ba35
Taste2 <stawiah17@gmail.com>
2023-09-11 10:33:51 +0000

    removed results

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -15,6 +15,6 @@ request
 })
 .on('complete', function () {
     response = JSON.parse(parts);
-    const count_ = response.results.films.length;
+    const count_ = response.films.length;
     console.log(count_);
 })
