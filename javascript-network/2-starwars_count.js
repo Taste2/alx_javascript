f8da5aa3f84486d823c11ba0e990dc15e6448252
Taste2 <stawiah17@gmail.com>
2023-09-11 01:58:59 +0000

    task2 added

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// import request
+const { isUtf8 } = require('buffer');
+const { parse } = require('path');
+const request = require('request');
+
+
+// pass cmd line argument
+const url = process.argv[2];
+
+parts = [];
+// make a GET request
+request
+.get(url+18, {encoding: 'utf-8'})
+.on('data', function (data) {
+    response = JSON.parse(data);
+    let count_ = 0;
+    for(let i = 1; 1 <= response.films.length; 1++) {
+        count_++;
+    }
+    console.log(count_);
+})
