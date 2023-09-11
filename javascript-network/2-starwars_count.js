106b36eb9639a13f7f8a6cca7b799c75d2a1705d
Taste2 <stawiah17@gmail.com>
2023-09-11 10:25:02 +0000

    store data in array

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -6,12 +6,15 @@ const request = require('request');
 
 // pass cmd line argument
 const url = process.argv[2];
-
+const parts = [];
 // make a GET request
 request
 .get(url+18, {encoding: 'utf-8'})
 .on('data', function (data) {
-    response = JSON.parse(data);
+    parts.push(data)
+})
+.on('complete', function () {
+    response = JSON.parse(parts);
     const count_ = response.results.films.length;
     console.log(count_);
 })
