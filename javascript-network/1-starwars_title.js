61a0adb193c2cf32bfa41bfaf87047f256548d2c
Taste2 <stawiah17@gmail.com>
2023-09-11 00:46:09 +0000

    parse json to object

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 // import request
+const { isUtf8 } = require('buffer');
 const { parse } = require('path');
 const request = require('request');
 
@@ -11,8 +12,8 @@ const url = 'https://swapi-api.alx-tools.com/api/films/'+id;
 
 // make a GET request
 request
-.get(url)
-.on('response', function (response) {
-    const data = response.JSON(parse)
-    console.log(data.title)
+.get(url, {encoding: 'utf-8'})
+.on('data', function (data) {
+    const response = JSON.parse(data)
+    console.log(response.title)
 })
