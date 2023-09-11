86bc1dc797f60ed59da0ee3efb2570616b20bfb4
Taste2 <stawiah17@gmail.com>
2023-09-11 02:07:16 +0000

    removed for loop

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -9,15 +9,11 @@ const request = require('request');
 // pass cmd line argument
 const url = process.argv[2];
 
-parts = [];
 // make a GET request
 request
 .get(url+18, {encoding: 'utf-8'})
 .on('data', function (data) {
     response = JSON.parse(data);
-    let count_ = 0;
-    for(let i = 1; i <= response.films.length; i++) {
-        count_++;
-    }
+    const count_ = response.films.length;
     console.log(count_);
 })
